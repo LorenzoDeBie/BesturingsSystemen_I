81314f09dee8651cafd4893b4f7a63b143e74579
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2019-05-28 19:39:33 +0200

    Initial commit voor Reeks 3

---
+++ Labo/Reeks 3/0.pl
@@ -0,0 +1,15 @@
+#Inverteer de hashes, zoals tijdens de inleidende les werd gedemonstreerd.
+%gewest=( "Antwerpen"      => "Vlaanderen", "Henegouwen"   => "Wallonie",
+          "Limburg"        => "Vlaanderen", "Namen"        => "Wallonie",
+          "Oost-Vlaanderen"=> "Vlaanderen", "Luik"         => "Wallonie",
+          "Vlaams-Brabant" => "Vlaanderen", "Luxemburg"    => "Wallonie",
+          "West-Vlaanderen"=> "Vlaanderen", "Waals-Brabant"=> "Wallonie");
+
+%provincie=( "Aalst"       => "Oost-Vlaanderen", "Brugge"  => "West-Vlaanderen",
+             "Dendermonde" => "Oost-Vlaanderen", "Ieper"   => "West-Vlaanderen",
+             "Eeklo"       => "Oost-Vlaanderen", "Oostende"=> "West-Vlaanderen",
+             "Oudenaarde"  => "Oost-Vlaanderen", "Kortrijk"=> "West-Vlaanderen",
+             "Sint-Niklaas"=> "Oost-Vlaanderen", "Gent"    => "Oost-Vlaanderen",
+             "Halle"       => "Vlaams-Brabant" , "Genk"    => "Limburg"        ,
+             "Leuven"      => "Vlaams-Brabant" , "Hasselt" => "Limburg"        ,
+             "Vilvoorde"   => "Vlaams-Brabant" , "Tongeren"=> "Limburg"        );
