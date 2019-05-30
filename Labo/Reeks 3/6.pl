2beb3caa730ef78735822ce4843aa3a32c6ca0df
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2019-05-30 20:49:50 +0200

    Oef 3.6 toegevoegd

---
+++ Labo/Reeks 3/6.pl
@@ -0,0 +1,9 @@
+=begin opgave
+In reeks 2 vraag 20 werden een paar technieken gehanteerd om de inhoud van een hash te tonen. 
+Je kan bijvoorbeeld een intermediare array invoeren waarin je de volledige hash opslaat, 
+om deze dan ineens uitprinten. Pas dit nu toe met behulp van een anonieme array, 
+om een expliciete intermediare array te vermijden.
+=end opgave
+=cut
+
+#deze vraag klopt niet meer, leerkrachten en cursussen updaten is toch niet alles...
