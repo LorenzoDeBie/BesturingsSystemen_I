c369c0c0acd07f89a017d99e2f56322de2750d15
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2019-05-30 20:51:34 +0200

    Oef 3.7 toegevoegd

---
+++ Labo/Reeks 3/7.pl
@@ -0,0 +1,10 @@
+=begin opgave
+Hoe kun je in een hash meerdere waarden per index opslaan ?
+=end opgave
+=cut
+
+#door als waarde een reference te gebruiken naar meerdere waarden
+
+%hash = (
+    "index1" => ["waarde1", "waarde2", "waarde3"]
+);
