b71b5d8813f0cbc7fe46b51e2ee07808abdb81b3
LorenzoDeBie <lorenzodebie@ndbsoft.be>
2019-05-30 20:53:59 +0200

    Oef 3.8 toegevoegd

---
+++ Labo/Reeks 3/8.pl
@@ -0,0 +1,6 @@
+=begin opgave
+In reeks 2 vraag 35 werd besproken hoe je een hash kon inverteren. Veralgemeen deze methode, zodat je nu ook toelaat dat de originele hash duplikaten vertoont.
+=end opgave
+=cut 
+
+#zie oefening 0
